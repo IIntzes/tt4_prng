5cdcc40e1cd31738df7c292ceb7c252caf4986c1
IoannisIn <intzes.ioannis@gmail.com>
2023-09-07 08:03:38 +0300

    Update tt_um_top.v

--- src/tt_um_top.v
+++ src/tt_um_top.v
@@ -16,8 +16,8 @@ module tt_um_top #( parameter MAX_COUNT = 24'd10_000_000 ) (
     wire EN = ena;
     wire [6:0] HEX0;
     assign uo_out[6:0] = HEX0;
-    wire clk1hz = 1'b0; //Output indication of the produced 1Hz clock.
-    assign uo_out[7] = clk1hz;
+    //wire clk1hz = 1'b0; //Output indication of the produced 1Hz clock.
+    //assign uo_out[7] = clk1hz;
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
     wire [6:0] HEX1;
