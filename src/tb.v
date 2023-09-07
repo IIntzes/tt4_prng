e7fa1d8bd9a24fdef3d6b55d49e3b86a6f26eb2e
IoannisIn <intzes.ioannis@gmail.com>
2023-09-07 09:38:10 +0300

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -14,7 +14,7 @@ module top_tb;
         .EN(EN),
         .CLK(CLK),
         .HEX0(HEX0),
-        .HEX1(HEX1),
+        .HEX1(HEX1)
     );
 
     // Clock generation
@@ -57,3 +57,50 @@ initial begin
     $display("Simulation complete.");
     $finish;
 end
+
+`default_nettype none
+`timescale 1ns/1ps
+
+/*
+this testbench just instantiates the module and makes some convenient wires
+that can be driven / tested by the cocotb test.py
+*/
+
+// testbench is controlled by test.py
+module tb ();
+
+    // this part dumps the trace to a vcd file that can be viewed with GTKWave
+    initial begin
+        $dumpfile ("tb.vcd");
+        $dumpvars (0, tb);
+        #1;
+    end
+
+    // wire up the inputs and outputs
+    reg  CLK = clk;
+    reg  reset = rst_n;
+    reg  EN = ena;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
+
+    wire [7:0] {HEX0, 1'b0} = uo_out;
+    wire [7:0] {HEX1, 1'b0} = uio_out;
+    wire [7:0] uio_oe;
+
+    tt_um_top (
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
+        );
+
+endmodule
