21e17dc390e91a628b19a988089e742a18f5e4c9
IoannisIn <intzes.ioannis@gmail.com>
2023-09-07 08:21:33 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,5 +3,5 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
-async def pwm(dut):
+async def lfsr_test(dut):
     dut._log.info("start")
